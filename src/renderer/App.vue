528fada6600f187ad064a795d36d3b9d1b9913b7
Eiichi Takebuchi <nanashi4129@gmail.com>
2018-03-28 23:55:18 +0900

    最初からやり直さないと駄目やわ

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+ 
+export default Vue.extend({
+    name: 'app',
+    props: {
+    },
+    components: { },
+    data() {
+    },
+    methods: {
+    },
+    created() {
+    },
+});
+ 
+ 
+<template lang="pug">
+div#app Hello Electron-Vue-Typescript!!
+</template>
+ 
+<style>
+</style>
